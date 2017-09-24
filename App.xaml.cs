ab8d4ec9c4047703e762622072fd79bf42321023
Андрей <andrewskobcov@yandex.ru>
2017-09-24 15:34:08 +0300

    Initial upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using PowerSupplyNotifier.Localizations;
+using PowerSupplyNotifier.Windows;
+
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.MessageBox;
+using PowerLineStatus = System.Windows.Forms.PowerLineStatus;
 
 namespace PowerSupplyNotifier
 {
@@ -12,5 +15,63 @@ namespace PowerSupplyNotifier
     /// </summary>
     public partial class App : Application
     {
+        public static NotifyIcon NotifyIcon { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            NotifyIcon = new NotifyIcon();
+
+            InitNotifyIcon(NotifyIcon);
+
+            if (SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline)
+                NotifyIcon.Icon = PowerSupplyNotifier.Properties.Resources.app_icon_battery;
+
+            SystemEvents.PowerModeChanged += SystemEventsOnPowerModeChanged;
+
+            //new Windows.SettingsWindow().Show();
+        }
+
+        private void InitNotifyIcon(NotifyIcon icon)
+        {
+            icon.Icon = PowerSupplyNotifier.Properties.Resources.app_icon;
+            icon.Visible = true;
+
+            icon.ContextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    new ToolStripMenuItem(StringResources.ShowSettingsWindow, null, (_, __) => SettingsWindow.ShowUnique()),
+                    new ToolStripMenuItem(StringResources.Exit, null, (_, __) => Shutdown(0))
+                }
+            };
+        }
+
+        private void SystemEventsOnPowerModeChanged(object sender, PowerModeChangedEventArgs args)
+        {
+            if (args.Mode == PowerModes.StatusChange)
+            {
+                if (SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline)
+                {
+                    NotifyIcon.Icon = PowerSupplyNotifier.Properties.Resources.app_icon_battery;
+
+                    MessageBox.Show(StringResources.PowerSupplyChangedToBattery, StringResources.PowerSupplyInformation, MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online)
+                {
+                    NotifyIcon.Icon = PowerSupplyNotifier.Properties.Resources.app_icon;
+
+                    MessageBox.Show(StringResources.PowerSupplyChangedToNetwork, StringResources.PowerSupplyInformation, MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            SystemEvents.PowerModeChanged -= SystemEventsOnPowerModeChanged;
+
+            NotifyIcon.Dispose();
+        }
     }
 }
---
+++ Windows/SettingsWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using NAudio.Wave;
+using PowerSupplyNotifier.Annotations;
+using PowerSupplyNotifier.Localizations;
+using PowerSupplyNotifier.Properties;
+
+namespace PowerSupplyNotifier.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window, INotifyPropertyChanged
+    {
+        private static SettingsWindow Instance { get; set; }
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            FillAudioDevices();
+        }
+
+        private void FillAudioDevices()
+        {
+            if (AudioDevices.Count > 0)
+                AudioDevices.Clear();
+            
+            var deviceCound = WaveOut.DeviceCount;
+
+            for (int i = 0; i < deviceCound; i++)
+                AudioDevices.Add(WaveOut.GetCapabilities(i));
+        }
+
+        public ObservableCollection<WaveOutCapabilities> AudioDevices { get; } = new ObservableCollection<WaveOutCapabilities>();
+
+        public string SoundFile
+        {
+            get => Settings.Default.SoundFile;
+            set
+            {
+                if (File.Exists(value))
+                {
+                    Settings.Default.SoundFile = value;
+                    Settings.Default.Save();
+                }
+                else
+                {
+                    MessageBox.Show(
+                        string.Format(StringResources.SoundFileNotFound, value), StringResources.Error,
+                        MessageBoxButton.OK, MessageBoxImage.Warning
+                    );
+                }
+
+                OnPropertyChanged(nameof(SoundFile));
+            }
+        }
+
+        public static void ShowUnique()
+        {
+            if (Instance == null)
+            {
+                Instance = new SettingsWindow();
+                Instance.Show();
+            }
+            else
+            {
+                Instance.Focus();
+            }
+        }
+
+        public void PlaySound(int deviceNumber, string fileName)
+        {
+            var waveReader = new WaveFileReader(fileName);
+            var waveOut = new WaveOut {DeviceNumber = deviceNumber};
+
+            waveOut.Init(waveReader);
+            waveOut.Play();
+        }
+
+        private void SettingsWindow_OnClosed(object sender, EventArgs e)
+        {
+            Instance = null;
+        }
+
+        private void ChooseSoundFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = StringResources.WavFilesFilter,
+                CheckFileExists = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                SoundFile = dialog.FileName;
+            }
+        }
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
